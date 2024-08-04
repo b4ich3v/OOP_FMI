e105b8cd5cf722ac082f2f5a7e64b4b9d772e41b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-08-04 17:26:27 +0300

    Create countOfLines.cpp

---
+++ Seminars/Seminar. 02/countOfLines.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+int countOfLines(const char* fileName)
+{
+
+	std::ifstream file(fileName);
+
+	if (!file.is_open()) 
+	{
+
+		std::cout << "Error";
+		return -1;
+
+	}
+
+	int count = 0;
+	char buff[1024];
+
+	while (file.getline(buff, 1024))
+	{
+
+		count++;
+
+	}
+
+	return count;
+
+}
+
+
+int main() 
+{
+
+	const char fileName[] = "dada.cpp";
+	std::cout << countOfLines(fileName);
+
+	return 0;
+
+}

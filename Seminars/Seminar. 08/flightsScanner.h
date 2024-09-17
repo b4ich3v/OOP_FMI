7ac20138a90219bbcfdcfda0c0e65e83f60c0e07
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-09-17 17:02:48 +0300

    Update flightsScanner.h

--- Seminars/Seminar. 08/flightsScanner.h
+++ Seminars/Seminar. 08/flightsScanner.h
@@ -3,6 +3,7 @@
 
 class FlightsScanner
 {
+
 	char inputFileName[1024] = "";
 	char outputFileName[1024] = "";
 	int sec = -1;
@@ -11,9 +12,11 @@ class FlightsScanner
 
 	struct FlightRecord
 	{
+
 		char origin[64];
 		char dest[64];
 		unsigned amount;
+
 	};
 
 	void handleAirport(char* airport) const;
@@ -21,7 +24,10 @@ class FlightsScanner
 	bool readAndExportData(std::ifstream& input);
 	void saveRecord(std::ofstream& ofs, const FlightRecord& record) const;
 
+
 public:
+
 	FlightsScanner(const char* input, const char* output, int scanInterval);
 	void run();
+
 };

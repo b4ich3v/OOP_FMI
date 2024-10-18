75f658fdf2798416553b46f906f9dd06015d6bfc
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 23:46:26 +0300

    Update CSVFileReader.h

--- Seminars/Seminar. 11/FileReadWrite/CSVFileReader.h
+++ Seminars/Seminar. 11/FileReadWrite/CSVFileReader.h
@@ -3,10 +3,13 @@
 
 class CSVFileReader : public FileReader
 {
+
 	static int getCharCount(std::ifstream& ifs, char ch);
 
 public:
+
 	CSVFileReader(const MyString& filePath);
 	int* read(size_t& size) const override;
+
 };
 

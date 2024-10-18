5b914e48b7fe57971ae4cbfa7bbd73505e2003e2
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 23:47:42 +0300

    Update FileWriter.h

--- Seminars/Seminar. 11/FileReadWrite/FileWriter.h
+++ Seminars/Seminar. 11/FileReadWrite/FileWriter.h
@@ -4,13 +4,16 @@
 class FileWriter
 {
 protected:
+
 	MyString filePath;
 
 public:
+
 	FileWriter(const MyString& filePath);
 
 	virtual void write(const int* arr, size_t size) const = 0;
 
 	virtual ~FileWriter() = default;
+
 };
 

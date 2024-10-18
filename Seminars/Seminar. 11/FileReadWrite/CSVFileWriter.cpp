66b9d7f0d22ecf485b8310b91282161927f5d92b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 23:46:44 +0300

    Update CSVFileWriter.cpp

--- Seminars/Seminar. 11/FileReadWrite/CSVFileWriter.cpp
+++ Seminars/Seminar. 11/FileReadWrite/CSVFileWriter.cpp
@@ -5,22 +5,30 @@ CSVFileWriter::CSVFileWriter(const MyString& filePath): FileWriter(filePath){}
 
 void CSVFileWriter::write(const int* arr, size_t size) const
 {
+	
 	std::ofstream ofs(filePath.c_str());
 
 	if (!ofs.is_open())
 	{
+		
 		throw std::exception("Cannot open file");
+		
 	}
 
 	for (int i = 0; i < size; i++)
 	{
+		
 		ofs << arr[i];
 
 		if (i != size - 1)
 		{
+			
 			ofs << ',';
+			
 		}
+		
 	}
 
 	ofs.close();
+	
 }

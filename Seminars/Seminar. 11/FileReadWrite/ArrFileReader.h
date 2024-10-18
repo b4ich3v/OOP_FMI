807fe16f0224c13c94c7cd312ed6665cbfd4e05d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 23:44:36 +0300

    Update ArrFileReader.h

--- Seminars/Seminar. 11/FileReadWrite/ArrFileReader.h
+++ Seminars/Seminar. 11/FileReadWrite/ArrFileReader.h
@@ -3,9 +3,11 @@
 
 class ArrFileReader: public FileReader
 {
+
 	static int getCharCount(std::ifstream& ifs, char ch);
 public:
 	ArrFileReader(const MyString& filePath);
 	int* read(size_t& size) const override;
+
 };
 

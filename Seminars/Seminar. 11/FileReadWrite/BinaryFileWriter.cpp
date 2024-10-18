e57e2045b64f48c030782fd41a764ed43fa9450b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 23:45:44 +0300

    Update BinaryFileWriter.cpp

--- Seminars/Seminar. 11/FileReadWrite/BinaryFileWriter.cpp
+++ Seminars/Seminar. 11/FileReadWrite/BinaryFileWriter.cpp
@@ -5,13 +5,17 @@ BinaryFileWriter::BinaryFileWriter(const MyString& filePath): FileWriter(filePat
 
 void BinaryFileWriter::write(const int* arr, size_t size) const
 {
+	
 	std::ofstream ofs(filePath.c_str(), std::ios::binary);
 
 	if (!ofs.is_open())
 	{
+		
 		throw std::exception("Cannot open file");
+		
 	}
 
 	ofs.write((const char*)arr, size * sizeof(int));
 	ofs.close();
+	
 }

7dde4bc21429952d25fb940c4ca5e8f0758856e2
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-02-17 22:50:18 +0200

    Create stringToNumber.cpp

---
+++ 2025/Seminars/Seminar. 01/stringToNumber.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+
+enum class Error
+{
+
+	Invalid,
+	Valid,
+	Nullptr,
+	EmptyText,
+	Overflow
+
+};
+
+struct ResultPair
+{
+public:
+
+	int number;
+	Error error;
+
+};
+
+int toDigit(char ch)
+{
+
+	return ch - '0';
+
+}
+
+bool isValid(char ch) 
+{
+
+	return ch >= '0' && ch <= '9';
+
+}
+
+ResultPair parse(const char* str) 
+{
+
+	if (str == nullptr)
+	{
+
+		return { 0, Error::Nullptr };
+
+	}
+
+	if (strlen(str) == 0) 
+	{
+
+		return { 0, Error::EmptyText };
+
+	}
+
+	int iter = 1;
+	bool isNegative = false;
+	int number = 0;
+	int counter = 0;
+
+	for (int i = 0; i < strlen(str) - 1; i++) iter *= 10;// 
+
+	if (*str == '-') 
+	{
+
+		isNegative = true;
+		str += 1;
+		iter /= 10;
+
+	}
+
+	while (*str != '\0')
+	{
+
+		if (!isValid(*str)) 
+		{
+
+			return { 0, Error::Invalid };
+
+		}
+		else
+		{
+
+			number += toDigit(*str) * iter;
+			iter /= 10;
+			counter |= number;
+			str += 1;
+
+			if ((counter ^ UINT32_MAX) == 0 && *str != '/0') 
+			{
+
+				return { 0, Error::Overflow };
+
+			}
+
+		}
+
+	}
+
+	if (isNegative)
+	{
+
+		number *= -1;
+		return { number, Error::Valid };
+
+	}
+
+	return { number, Error::Valid };
+
+}
+
+int main() 
+{
+
+	char str[5] = "-123";
+	ResultPair result = parse(str);
+
+	std::cout << result.number << " ";
+
+	switch (result.error) 
+	{
+
+	case Error::Invalid: std::cout << "invalid"; break;
+	case Error::Valid: std::cout << "valid"; break;
+	case Error::Nullptr: std::cout << "nullptr"; break;
+	case Error::Overflow: std::cout << "overflow"; break;
+	case Error::EmptyText: std::cout << "emptyText"; break;
+
+	}
+
+	return 0;
+
+}

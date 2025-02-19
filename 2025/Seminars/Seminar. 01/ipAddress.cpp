ee24bb50e06cdcf08c638160742b78811cc07888
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-02-20 00:03:28 +0200

    Create ipAddress.cpp

---
+++ 2025/Seminars/Seminar. 01/ipAddress.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+union ipAddress
+{
+
+	int number;
+	uint8_t parts[4];
+
+};
+
+int main()
+{
+
+	ipAddress ip;
+	ip.number = 2563931973;
+	std::cout << (unsigned int)ip.parts[0] << "." << (unsigned int)ip.parts[1]
+		      << "." << (unsigned int)ip.parts[2] << "." << (unsigned int)ip.parts[3];
+
+	return 0;
+
+}

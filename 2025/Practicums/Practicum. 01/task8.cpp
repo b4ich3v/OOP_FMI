e2708e03c4af866a2aa1cdf5c8262487dcf50348
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-02-20 01:20:14 +0200

    Update task8.cpp

--- 2025/Practicums/Practicum. 01/task8.cpp
+++ 2025/Practicums/Practicum. 01/task8.cpp
@@ -8,7 +8,7 @@ public:
 
 };
 
-struct Rib
+struct Edge
 {
 public:
 
@@ -21,9 +21,9 @@ struct Graph
 {
 public:
 
-	Rib* ribs;
+	Edge* edges;
 	int countOfNodes;
-	int countOfRibs;
+	int countOfEdges;
 
 };
 
@@ -31,18 +31,18 @@ Graph readGraph()
 {
 
 	Graph result = { nullptr, 0 ,0 };
-	std::cin >> result.countOfRibs;
-	result.ribs = new Rib[result.countOfRibs];
+	std::cin >> result.countOfEdges;
+	result.edges = new Edge[result.countOfEdges];
 	std::cin.ignore();
 
-	char** allNames = new char* [result.countOfRibs * 2];
-	bool* helper = new bool[result.countOfRibs * 2] {false};
+	char** allNames = new char* [result.countOfEdges * 2];
+	bool* helper = new bool[result.countOfEdges * 2] {false};
 	int index = 0;
 	int counter = 0;
 
-	for (int i = 0; i < result.countOfRibs * 2; i++) allNames[i] = new char[11];
+	for (int i = 0; i < result.countOfEdges * 2; i++) allNames[i] = new char[11];
 
-	for (int i = 0; i < result.countOfRibs; i++)
+	for (int i = 0; i < result.countOfEdges; i++)
 	{
 
 		Node node1;
@@ -55,15 +55,15 @@ Graph readGraph()
 		strncpy(allNames[index + 1], node2.name, 11);
 		index += 2;
 
-		Rib currentRib = { node1 ,node2 };
-		result.ribs[i] = currentRib;
+		Edge currentRib = { node1 ,node2 };
+		result.edges[i] = currentRib;
 
 	}
 
-	for (int i = 0; i < result.countOfRibs * 2; i++)
+	for (int i = 0; i < result.countOfEdges * 2; i++)
 	{
 
-		for (int j = i + 1; j < result.countOfRibs * 2; j++)
+		for (int j = i + 1; j < result.countOfEdges * 2; j++)
 		{
 
 			if (!strcmp(allNames[i], allNames[j]) && !helper[j])
@@ -78,7 +78,7 @@ Graph readGraph()
 
 	}
 
-	for (int i = 0; i < result.countOfRibs * 2; i++)
+	for (int i = 0; i < result.countOfEdges * 2; i++)
 	{
 
 		delete[] allNames[i];
@@ -88,7 +88,7 @@ Graph readGraph()
 	delete[] helper;
 	delete[] allNames;
 
-	result.countOfNodes = result.countOfRibs * 2 - counter;
+	result.countOfNodes = result.countOfEdges * 2 - counter;
 	return result;
 
 }
@@ -96,55 +96,55 @@ Graph readGraph()
 void free(Graph& graph)
 {
 
-	delete[] graph.ribs;
-	graph.ribs = nullptr;
+	delete[] graph.edges;
+	graph.edges = nullptr;
 	graph.countOfNodes = 0;
-	graph.countOfRibs = 0;
+	graph.countOfEdges = 0;
 
 }
 
-Graph addRib(const Node& node1, const Node& node2, Graph& graph)
+Graph addEdge(const Node& node1, const Node& node2, Graph& graph)
 {
 
-	Rib newRib = { node1 , node2 };
+	Edge newEdge = { node1 , node2 };
 	Graph result;
 	result.countOfNodes = graph.countOfNodes;
-	result.countOfRibs = graph.countOfRibs + 1;
-	result.ribs = new Rib[graph.countOfRibs + 1];
+	result.countOfEdges = graph.countOfEdges + 1;
+	result.edges = new Edge[graph.countOfEdges + 1];
 
-	for (int i = 0; i < graph.countOfRibs; i++)
+	for (int i = 0; i < graph.countOfEdges; i++)
 	{
 
-		result.ribs[i] = graph.ribs[i];
+		result.edges[i] = graph.edges[i];
 
 	}
 
-	delete[] graph.ribs;
-	result.ribs[graph.countOfRibs] = newRib;
+	delete[] graph.edges;
+	result.edges[graph.countOfEdges] = newEdge;
 	return result;
 
 }
 
-Graph removeRib(const Rib& rib, Graph& graph)
+Graph removeEdge(const Edge& edge, Graph& graph)
 {
 
 	Graph result;
 	result.countOfNodes = graph.countOfNodes;
-	result.countOfRibs = graph.countOfRibs - 1;
-	result.ribs = new Rib[graph.countOfRibs - 1];
+	result.countOfEdges = graph.countOfEdges - 1;
+	result.edges = new Edge[graph.countOfEdges - 1];
 
 	int index = 0;
 
-	for (int i = 0; i < graph.countOfRibs; i++)
+	for (int i = 0; i < graph.countOfEdges; i++)
 	{
 
-		if (!strcmp(rib.node1.name, graph.ribs[i].node1.name)) continue;
-		result.ribs[index] = graph.ribs[i];
+		if (!strcmp(edge.node1.name, graph.edges[i].node1.name)) continue;
+		result.edges[index] = graph.edges[i];
 		index += 1;
 
 	}
 
-	delete[] graph.ribs;
+	delete[] graph.edges;
 	return result;
 
 }
@@ -155,11 +155,11 @@ void exitAndEnter(const Node& node, const Graph& graph)
 	int exit = 0;
 	int enter = 0;
 
-	for (int i = 0; i < graph.countOfRibs; i++)
+	for (int i = 0; i < graph.countOfEdges; i++)
 	{
 
-		if (!strcmp(node.name, graph.ribs[i].node1.name)) exit += 1;
-		else if (!strcmp(node.name, graph.ribs[i].node2.name)) enter += 1;
+		if (!strcmp(node.name, graph.edges[i].node1.name)) exit += 1;
+		else if (!strcmp(node.name, graph.edges[i].node2.name)) enter += 1;
 
 	}
 
@@ -170,18 +170,18 @@ void exitAndEnter(const Node& node, const Graph& graph)
 bool isGraphFull(const Graph& graph)
 {
 
-	return graph.countOfRibs == graph.countOfNodes * (graph.countOfNodes - 1);
+	return graph.countOfEdges == graph.countOfNodes * (graph.countOfNodes - 1);
 
 }
 
-bool checkIfRibExist(const Rib& rib, const Graph& graph)
+bool checkIfEdgeExist(const Edge& edge, const Graph& graph)
 {
 
-	for (int i = 0; i < graph.countOfRibs; i++)
+	for (int i = 0; i < graph.countOfEdges; i++)
 	{
 
-		if (!strcmp(rib.node1.name, graph.ribs[i].node1.name) &&
-			!strcmp(rib.node2.name, graph.ribs[i].node2.name))
+		if (!strcmp(edge.node1.name, graph.edges[i].node1.name) &&
+			!strcmp(edge.node2.name, graph.edges[i].node2.name))
 		{
 
 			return true;

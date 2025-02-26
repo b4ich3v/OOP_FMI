91b9856f39a196c94a5a19431d3af011927188d6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-02-26 14:42:34 +0200

    Update task11.cpp

--- 2025/Practicums/Practicum. 02/task11.cpp
+++ 2025/Practicums/Practicum. 02/task11.cpp
@@ -11,26 +11,36 @@ void concatTwoFilesAndReplace(const char* fileName1, const char* fileName2, cons
     if (!file1.is_open() || !file2.is_open() || !file3.is_open()) return;
 
     char buffer[1024];
+    bool isFirstRow = true;
 
     while (true)
     {
+
+        if(!isFirstRow) file3 << std::endl;
 
         file1.getline(buffer, 1024);
         file3 << buffer;
-        file3 << std::endl;
 
         if (file1.eof()) break;
 
+        isFirstRow = false;
+
     }
+
+    isFirstRow = true;
+    file3 << " ";
 
     while (true)
     {
 
+        if (!isFirstRow) file3 << std::endl;
+
         file2.getline(buffer, 1024);
         file3 << buffer;
-        file3 << std::endl;
 
         if (file2.eof()) break;
+
+        isFirstRow = false;
 
     }
 
@@ -40,10 +50,35 @@ void concatTwoFilesAndReplace(const char* fileName1, const char* fileName2, cons
 
 }
 
+void addSymbolIfNeeded(const char* fileName, char ch)
+{
+
+    std::fstream file(fileName);
+
+    if (!file.is_open()) return;
+
+    char buffer[1024];
+
+    while (true)
+    {
+
+        file.get(buffer, 1024, ' ');
+        file.put(ch);
+
+        if (file.eof()) break;
+
+    }
+
+    file.close();
+
+}
+
 int main()
 {
 
     concatTwoFilesAndReplace("../test1.txt", "../test2.txt", "../result.txt");
+    addSymbolIfNeeded("../result.txt", ',');
+
     return 0;
 
 }

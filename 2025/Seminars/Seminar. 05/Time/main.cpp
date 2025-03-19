5534c74763f0d27c03b24bbcb51fbf503bbfbe86
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-20 01:13:15 +0200

    Update main.cpp

--- 2025/Seminars/Seminar. 05/Time/main.cpp
+++ 2025/Seminars/Seminar. 05/Time/main.cpp
@@ -1,1 +1,27 @@
+#include "Time.h"
 
+int main()
+{
+
+	Time time1(21, 20, 58);
+	Time time2(5, 9, 19);
+
+	time1.printTime();
+	time2.printTime();
+
+	Time leftToMidnight = time1.timeLeftToMidnight();
+	leftToMidnight.printTime();
+
+	Time diffTime = time1.differenceOfTwo(time2);
+	diffTime.printTime();
+
+	std::cout << time1.isDinnerTime() << std::endl;
+	std::cout << time2.isPartyTime() << std::endl;
+
+	time1.tick();
+	time1.tick();
+	time1.printTime();
+
+	return 0;
+
+}

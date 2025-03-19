64f7332772d0cb551a93fd2aa1d85679b428d692
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-20 01:07:03 +0200

    Update main.cpp

--- 2025/Seminars/Seminar. 05/Interval/main.cpp
+++ 2025/Seminars/Seminar. 05/Interval/main.cpp
@@ -1,1 +1,23 @@
+#include "Interval.h"
 
+int main()
+{
+
+	Interval interval1(-1, 15);
+	Interval interval2(-5, 9);
+
+	interval1.intersection(interval2);
+	interval1.print();
+
+	Interval interval3(1, 64);
+	std::cout << interval3.areStartAndEndAPOT() << std::endl;
+	std::cout << interval3.countOfNumbersWDDInInterval() << std::endl;
+	std::cout << interval3.countOfPalindromesInInterval() << std::endl;
+	std::cout << interval3.countOfPrimesInInterval() << std::endl;
+
+	std::cout << interval3.isSubinterval(Interval(5, 16)) << std::endl;
+	std::cout << interval3.isInInterval(27) << std::endl;
+
+	return 0;
+
+}

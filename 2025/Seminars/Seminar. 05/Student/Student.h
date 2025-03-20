675c72ec19157b8f1feaf119f66dcc5d3b02df85
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-20 02:13:38 +0200

    Update Student.h

--- 2025/Seminars/Seminar. 05/Student/Student.h
+++ 2025/Seminars/Seminar. 05/Student/Student.h
@@ -1,1 +1,57 @@
+#include <cstring>
+#include <iostream>
+#pragma once
 
+namespace HELPERS
+{
+
+    const int MAX_SIZE_QUOTE = 30;
+
+}
+
+class Student
+{
+private:
+
+    char* name = nullptr;
+    char quote[HELPERS::MAX_SIZE_QUOTE + 1] = "DEFAULT";
+
+    double* grades = nullptr;
+    int size = 0;
+    int capacity = 8;
+
+    void setName(const char* name);
+
+    void setQuote(const char* quote);
+
+    void setGrades(const double* grades, int size);
+
+    void free();
+
+    void copyFrom(const Student& other);
+
+    void resize(int newCapacity);
+
+public:
+
+    Student();
+
+    Student(const char* name, const char* quote, const double* grades, int size);
+
+    Student(const Student& other);
+
+    Student& operator = (const Student& other);
+
+    ~Student();
+
+    void addGrade(double newGrade);
+
+    const char* getName() const;
+
+    const char* getQuote() const;
+
+    const double* getGrades() const;
+
+    void printStudent() const;
+
+};

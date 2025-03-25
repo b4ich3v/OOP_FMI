9f3bdc1c19a765c00ecb0387b6c237e50776a504
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-25 15:38:38 +0200

    Create main.cpp

---
+++ 2025/Seminars/Seminar. 06/ComplexNumber/main.cpp
@@ -0,0 +1,29 @@
+#include "ComplexNumber.h"
+
+int main()
+{
+
+	ComplexNumber number(1, 1);
+	ComplexNumber number1(1, 1);
+	ComplexNumber number2(2, -11);
+	ComplexNumber number3(3, 9);
+	ComplexNumber number4(-6, 1);
+
+	number1 *= number2;
+	number1.printNumber();
+
+	number2 += number3;
+	number2.printNumber();
+
+	number3 /= number1;
+	number3.printNumber();
+
+	number4 -= number3;
+	number4.printNumber();
+
+	ComplexNumber number1UpTo7 = number ^ (-3);
+	number1UpTo7.printNumber();
+
+	return 0;
+
+}

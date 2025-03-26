948c9daa4d05693af5ac66e6ab8f817a0eb588b6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-26 10:47:35 +0200

    Create Vector.h

---
+++ 2025/Seminars/Seminar. 06/Vector/Vector.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#pragma once
+
+class Vector
+{
+private:
+
+	int* array = nullptr;
+	int size = 0;
+	int capacity = 8;
+
+	void copyFrom(const Vector& other);
+
+	void free();
+
+	void resize(int newCapacity);
+
+	void setArray(int* array, int size);
+
+public:
+
+	Vector();
+
+	Vector(const Vector& other);
+
+	Vector(int* array, int size);
+
+	Vector(int size);
+
+	Vector& operator = (const Vector& other);
+
+	Vector& operator += (const Vector& other);
+
+	Vector& operator -= (const Vector& other);
+
+	Vector& operator *= (double scalar);
+
+	int& operator [] (int index);
+
+	const int operator [] (int index) const;
+
+	friend std::istream& operator >> (std::istream& is, Vector& vector);
+
+	friend std::ostream& operator << (std::ostream& os, const Vector& vector);
+
+	~Vector();
+
+	void push(int number);
+
+	void pop();
+
+	int getSize() const;
+
+	bool isEmpty() const;
+
+};
+
+Vector operator + (const Vector& left, const Vector& right);
+
+Vector operator - (const Vector& left, const Vector& right);
+
+Vector operator * (const Vector& vector, double scalar);

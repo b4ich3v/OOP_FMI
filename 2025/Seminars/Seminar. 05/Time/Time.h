db212d7b479771dd9d79fac8f08c5d62e1e97c05
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-28 01:46:55 +0200

    Update Time.h

--- 2025/Seminars/Seminar. 05/Time/Time.h
+++ 2025/Seminars/Seminar. 05/Time/Time.h
@@ -2,28 +2,29 @@
 #include <iostream>
 #include <cstddef>
 #include <cstdint>
+#include <cmath>
 
 namespace HELPERS
 {
 
-    const char SEPARATOR[2] = ":";
-    const int SECONDS_IN_ONE_HOUR = 3600;
-    const int MINUTES_IN_ONE_HOUR = 60;
-    const int SECONDS_IN_ONE_MINUTE = 60;
+    const char SEPARATOR[2]          = ":";
+    const int SECONDS_IN_ONE_HOUR    = 3600;
+    const int MINUTES_IN_ONE_HOUR    = 60;
+    const int SECONDS_IN_ONE_MINUTE  = 60;
 
-    const int DINNER_HOURS_LOWER = 20;
-    const int DINNER_MINUTES_LOWER = 30;
-    const int DINNER_SECONDS_LOWER = 0;
-    const int DINNER_HOURS_UPPER = 22;
-    const int DINNER_MINUTES_UPPER = 0;
-    const int DINNER_SECONDS_UPPER = 0;
+    const int DINNER_HOURS_LOWER     = 20;
+    const int DINNER_MINUTES_LOWER   = 30;
+    const int DINNER_SECONDS_LOWER   = 0;
+    const int DINNER_HOURS_UPPER     = 22;
+    const int DINNER_MINUTES_UPPER   = 0;
+    const int DINNER_SECONDS_UPPER   = 0;
 
-    const int PARTY_HOURS_LOWER = 23;
-    const int PARTY_MINUTES_LOWER = 30;
-    const int PARTY_SECONDS_LOWER = 0;
-    const int PARTY_HOURS_UPPER = 6;
-    const int PARTY_MINUTES_UPPER = 0;
-    const int PARTY_SECONDS_UPPER = 0;
+    const int PARTY_HOURS_LOWER      = 23;
+    const int PARTY_MINUTES_LOWER    = 30;
+    const int PARTY_SECONDS_LOWER    = 0;
+    const int PARTY_HOURS_UPPER      = 6;
+    const int PARTY_MINUTES_UPPER    = 0;
+    const int PARTY_SECONDS_UPPER    = 0;
 
     bool isOneDigit(int number);
 

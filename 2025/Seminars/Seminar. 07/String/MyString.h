0d7f3d9344699dc77b5274555a3b2901e7c8c1d4
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-31 01:31:33 +0300

    Update and rename String.h to MyString.h

---
+++ 2025/Seminars/Seminar. 07/String/MyString.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+#pragma once
+
+class MyString
+{
+private:
+
+	char* data = nullptr;
+	int size = 0;
+	int capacity = 8;
+
+	void free();
+
+	void copyFrom(const MyString& other);
+
+	void resize(int newCapacity);
+
+public:
+
+	MyString();
+
+	MyString(const char* data);
+
+	MyString(const MyString& other);
+
+	MyString& operator = (const MyString& other);
+
+	MyString& operator += (const MyString& other);
+
+	~MyString();
+
+	char& operator [] (int index);
+
+	const char operator [] (int index) const;
+
+	const char* getData() const;
+
+	int getSize() const;
+
+	friend std::ostream& operator << (std::ostream& os, const MyString& str);
+
+	friend std::istream& operator >> (std::istream& is, MyString& str);
+
+};
+
+bool operator >= (const MyString& str1, const MyString& str2);
+
+bool operator <= (const MyString& str1, const MyString& str2);
+
+bool operator > (const MyString& str1, const MyString& str2);
+
+bool operator < (const MyString& str1, const MyString& str2);
+
+bool operator == (const MyString& str1, const MyString& str2);
+
+bool operator != (const MyString& str1, const MyString& str2);
+
+MyString operator + (const MyString& str1, const MyString& str2);

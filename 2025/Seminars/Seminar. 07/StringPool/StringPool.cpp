4a3f787282265688ee7ead14a3d50f5eb839096e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-31 02:41:46 +0300

    Update StringPool.cpp

--- 2025/Seminars/Seminar. 07/StringPool/StringPool.cpp
+++ 2025/Seminars/Seminar. 07/StringPool/StringPool.cpp
@@ -1,277 +1,33 @@
+#include <iostream>
 #include "StringPool.h"
 
-void Node::allocateData(const char* data) 
+int main() 
 {
 
-	if (data == nullptr) 
-	{
-
-		this->data = nullptr;
-		this->size = 0;
-		this->refCounter = 0;
-
-		return;
-
-	}
-
-	this->size = strlen(data);
-	this->data = new char[size + 1];
-	strncpy(this->data, data, size);
-	this->data[size] = '\0';
-	this->refCounter = 1;
-
-}
-
-Node& Node::operator=(const Node& other)
-{
-
-	if (this == &other) return *this;
-		
-	releaseData();
-
-	if (other.data != nullptr)
-	{
-
-		size = other.size;
-		data = new char[size + 1];
-		strcpy(data, other.data);  
-		refCounter = other.refCounter;
-
-	}
-	else
-	{
-
-		data = nullptr;
-		size = 0;
-		refCounter = 0;
-
-	}
-
-	return *this;
-
-}
-
-const char* Node::getData() const 
-{
-
-	return data;
-
-}
-
-int StringPool::getFirstFreeIndex() const 
-{
-
-	for (int i = 0; i < capacity; i++)
-	{
-
-		if (nodes[i].refCounter == 0) return i;
-
-	}
-
-	return -1;
-
-}
-
-int StringPool::getCurrentIndex(const char* str) const
-{
-
-	for (int i = 0; i < countOfNodes; i++)
-	{
-
-		const char* nodeData = nodes[i].getData();
-		if (nodeData != nullptr && strcmp(nodeData, str) == 0) return i;
-			
-	}
-
-	return -1;
-
-}
-
-void Node::releaseData() 
-{
-
-	delete[] data;
-	data = nullptr;
-	refCounter = 0;
-	size = 0;
-
-}
-
-void StringPool::free()
-{
-
-	for (int i = 0; i < countOfNodes; i++)
-	{
-
-		nodes[i].releaseData();
-
-	}
-
-	delete[] nodes;
-	nodes = nullptr;
-
-}
-
-void StringPool::resize(int newCapacity)
-{
-
-	if (newCapacity <= capacity) return;
-
-	Node* newNodes = new Node[newCapacity];
-
-	for (int i = 0; i < countOfNodes; i++)
-	{
-
-		newNodes[i].allocateData(nodes[i].getData());
-		newNodes[i].refCounter = nodes[i].refCounter;
-
-	}
-
-	for (int i = 0; i < countOfNodes; i++)
-	{
-
-		nodes[i].releaseData();
-
-	}
-
-	delete[] nodes;
-	nodes = newNodes;
-	capacity = newCapacity;
-
-}
-
-void StringPool::addToPool(int index, const char* str) 
-{
-
-	if (index == -1)
-	{
-
-		resize(capacity * 2);
-		index = countOfNodes;
-
-	}
-
-	nodes[index].allocateData(str);
-	nodes[index].refCounter = 1;
-	countOfNodes += 1;
-
-}
-
-const char* StringPool::removeFromPool(int index) 
-{
-
-	int size = nodes[index].size;
-	char* result = new char[size + 1];
-	const char* ptr = nodes[index].getData();
-
-	strncpy(result, ptr, size);
-	result[size] = '\0';
-
-	nodes[index].releaseData();
-	nodes[index].refCounter = 0;
-	countOfNodes -= 1;
-
-	return result;
-
-}
-
-int StringPool::getCountOfNodes() const 
-{
-
-	return countOfNodes;
-
-}
-
-void StringPool::chainString(const char* str)
-{
-
-	int index = getCurrentIndex(str);
-
-	if (index != -1) nodes[index].refCounter += 1;
-	else
-	{
-		
-		if (countOfNodes >= capacity) resize(capacity * 2);
-
-		nodes[countOfNodes].allocateData(str);
-		nodes[countOfNodes].refCounter = 1;
-		countOfNodes += 1;
-
-	}
-
-}
-
-const char* StringPool::unchainString(const char* str)
-{
-
-	int index = getCurrentIndex(str);
-	if (index == -1) return nullptr;  
-
-	if (nodes[index].refCounter > 1)
-	{
-
-		nodes[index].refCounter -= 1;
-		return nodes[index].getData();
-
-	}
-	else
-	{
-
-		int size = nodes[index].size;
-		char* result = new char[size + 1];
-		strncpy(result, nodes[index].getData(), size);
-		result[size] = '\0';
-
-		nodes[index].releaseData();
-
-		if (index != countOfNodes - 1)
-		{
-
-			nodes[index] = nodes[countOfNodes - 1];
-
-		}
-
-		countOfNodes -= 1;
-
-		return result;
-
-	}
-
-}
-
-StringPool::StringPool() 
-{
-
-	capacity = 8;
-	countOfNodes = 0;
-	nodes = new Node[capacity];
-
-}
-
-StringPool::~StringPool() 
-{
-
-	free();
-
-}
-
-void StringPool::printPool() const 
-{
-
-	for (int i = 0; i < countOfNodes; i++)
-	{
-
-		for (int j = 0; j < nodes[i].refCounter; j++)
-		{
-
-			std::cout << nodes[i].getData() << " ";
-
-		}
-
-		std::cout << std::endl;
-
-	}
-
-	std::cout << std::endl;
+    StringPool pool;
+
+    pool.chainString("Hello");
+    pool.chainString("World");
+    pool.chainString("Hello");  
+    pool.printPool();
+
+    std::cout << pool.getCountOfNodes() << std::endl;
+
+    const char* str1 = pool.unchainString("Hello");
+    std::cout << str1 << std::endl << std::endl;
+    pool.printPool();
+
+    const char* str2 = pool.unchainString("Hello");
+    std::cout << str2 << std::endl << std::endl;
+    pool.printPool();
+
+    const char* str3 = pool.unchainString("World");
+    std::cout << str3 << std::endl;
+    std::cout << pool.getCountOfNodes() << std::endl;
+    pool.printPool();
+
+    delete[] str2;
+    delete[] str3;
+    return 0;
 
 }

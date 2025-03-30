f7585e3e663fb673efccc0bfd9ca178ec7ad80b6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-31 02:36:07 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 07/StringPool/main.cpp
+++ 2025/Seminars/Seminar. 07/StringPool/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include "StringPool.h"
 
+int main() 
+{
+
+    StringPool pool;
+
+    pool.chainString("Hello");
+    pool.chainString("World");
+    pool.chainString("Hello");  
+
+    std::cout << pool.getCountOfNodes() << std::endl;
+
+    const char* str1 = pool.unchainString("Hello");
+    std::cout << str1 << std::endl;
+
+
+    const char* str2 = pool.unchainString("Hello");
+    std::cout << str2 << std::endl;
+ 
+
+    const char* str3 = pool.unchainString("World");
+    std::cout << str3 << std::endl;
+    std::cout << pool.getCountOfNodes() << std::endl;
+
+    delete[] str2;
+    delete[] str3;
+    return 0;
+
+}

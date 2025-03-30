2c45ad6d0a437939f1ce6ce9e3ae41fe8d92e9c3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-31 02:04:59 +0300

    Update StringPool.h

--- 2025/Seminars/Seminar. 07/StringPool/StringPool.h
+++ 2025/Seminars/Seminar. 07/StringPool/StringPool.h
@@ -1,1 +1,59 @@
+#include <iostream>
+#include <cstring>
+#pragma once
 
+struct Node 
+{
+public:
+
+	char* data = nullptr;
+	int size = 0;
+	int refCounter = 0;
+
+	Node() = default;
+
+	const char* getData() const;
+
+	void allocateData(const char* data);
+
+	void releaseData();
+
+};
+
+class StringPool
+{
+private:
+
+	Node* nodes = nullptr;
+	int countOfNodes = 0;
+	int capacity = 8;
+
+	void free();
+
+	void resize(int newCapacity);
+
+	int getFirstFreeIndex() const;
+
+	bool alreadyInPool(const char* str) const;
+
+	int getCurrentIndex(const char* str) const;
+
+	void addToPool(int index, const char* str);
+
+	const char* removeFromPool(int index);
+
+public:
+
+	StringPool();
+
+	StringPool(const StringPool& other) = delete;
+
+	StringPool& operator = (const StringPool) = delete;
+
+	~StringPool();
+
+	void chainString(const char* str);
+
+	const char* unchainString(const char* str);
+
+};

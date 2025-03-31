c14aa1d32a8f668c68a4f9aaf22534d4510c814b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-01 01:01:41 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 07/ImmutableString/main.cpp
+++ 2025/Seminars/Seminar. 07/ImmutableString/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "ImmutableString.h"
 
+int main() 
+{
+    
+    ImmutableString s1("Hello, world!");
+    ImmutableString s2("Goodbye!");
+
+    std::cout << "s1: " << s1 << std::endl;
+    std::cout << "s2: " << s2 << std::endl;
+
+    if (s1 < s2) std::cout << s1 << " is less than " << s2 << std::endl;
+    else if (s1 == s2) std::cout << s1 << " is equal to " << s2 << std::endl;
+    else std::cout << s1 << " is greater than " << s2 << std::endl;
+
+    ImmutableString s3(s1);
+    std::cout << s3 << std::endl;
+    std::cout << s1[0] << std::endl;
+
+    return 0;
+
+}

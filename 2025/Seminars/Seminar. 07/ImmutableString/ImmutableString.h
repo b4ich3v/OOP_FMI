a5e7b41e011a29678336c71776d51314e49dd1af
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-01 01:01:10 +0300

    Update ImmutableString.h

--- 2025/Seminars/Seminar. 07/ImmutableString/ImmutableString.h
+++ 2025/Seminars/Seminar. 07/ImmutableString/ImmutableString.h
@@ -1,1 +1,48 @@
+#include "StringPool.h"
+#pragma once
 
+class ImmutableString
+{
+private:
+
+	char* data = nullptr;
+	int size = 0;
+	static StringPool pool;
+
+	void copyFrom(const ImmutableString& data);
+
+	void free();
+
+public:
+
+	ImmutableString() = delete;
+
+	ImmutableString(const char* data);
+
+	ImmutableString(const ImmutableString& other);
+
+	ImmutableString& operator = (const ImmutableString& other) = delete;
+
+	~ImmutableString();
+
+	int getSize() const;
+
+	const char& operator [] (int index) const;
+
+	const char* getData() const;
+
+	friend std::ostream& operator << (std::ostream& os, const ImmutableString& other);
+
+};
+
+bool operator < (const ImmutableString& lhs, const ImmutableString& rhs);
+
+bool operator <= (const ImmutableString& lhs, const ImmutableString& rhs);
+
+bool operator >= (const ImmutableString& lhs, const ImmutableString& rhs);
+
+bool operator > (const ImmutableString& lhs, const ImmutableString& rhs);
+
+bool operator == (const ImmutableString& lhs, const ImmutableString& rhs);
+
+bool operator != (const ImmutableString& lhs, const ImmutableString& rhs);

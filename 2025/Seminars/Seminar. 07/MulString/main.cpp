a87976e5916e8562f970ac8a71fdc0dd4c7de90f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-02 16:13:05 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 07/MulString/main.cpp
+++ 2025/Seminars/Seminar. 07/MulString/main.cpp
@@ -1,1 +1,26 @@
+#include "MulString.h"
+
+int main()
+{
+
+	MyString str1("Yoan");
+	MyString str2(" Baychev");
+
+	str1 += str2;
+
+	MulString s1(str1);
+	MulString s2("ABCDEFrrrru1111ahjdaaaaaaahu192");
+	std::cout << s1;
+	
+	MulString result1 = s1 % s2;
+	std::cout << result1;
+	std::cout << s1 % " ";
+
+	MulString s3('4');
+	MulString result2 = s3 * 3;
+	std::cout << result2;
+
+	return 0;
+
+}
 

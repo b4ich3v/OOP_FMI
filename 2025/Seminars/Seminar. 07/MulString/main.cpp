53f60cfe41535792379c4142384c918966580573
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-02 16:48:03 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 07/MulString/main.cpp
+++ 2025/Seminars/Seminar. 07/MulString/main.cpp
@@ -11,16 +11,18 @@ int main()
 	MulString s1(str1);
 	MulString s2("ABCDEFrrrru1111ahjdaaaaaaahu192");
 	std::cout << s1;
-	
+
 	MulString result1 = s1 % s2;
 	std::cout << result1;
-	std::cout << s1 % " ";
+	std::cout << s1 % MulString(" ");
 
 	MulString s3('4');
 	MulString result2 = s3 * 3;
 	std::cout << result2;
 
+	MulString s4("Shte slusham ");
+	std::cout << s4 * 3;
+
 	return 0;
 
 }
-

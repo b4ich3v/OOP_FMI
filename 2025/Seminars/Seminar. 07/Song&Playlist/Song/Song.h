9afcc05f3be9d2a5e3e8c5de919c9f07eb84bee7
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-02 12:58:50 +0300

    Update Song.h

--- 2025/Seminars/Seminar. 07/Song&Playlist/Song/Song.h
+++ 2025/Seminars/Seminar. 07/Song&Playlist/Song/Song.h
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <cstring>
+#pragma once
 
+class Song
+{
+private:
+
+	char* name = nullptr;
+	char* contractor = nullptr;
+	double duration = 0;
+
+	void free();
+
+	void copyFrom(const Song& other);
+
+public:
+
+	Song() = default;
+
+	Song(const char* name, const char* contractor, double duration);
+
+	Song(const Song& other);
+
+	Song& operator = (const Song& other);
+
+	~Song();
+
+	double getDuration() const;
+
+	const char* getName() const;
+
+	const char* getContractor() const;
+
+	friend std::ostream& operator << (std::ostream& os, const Song& song);
+
+	friend std::istream& operator >> (std::istream& os, Song& song);
+
+};

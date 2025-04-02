5b77ded139405617ea10ccc86f306f5b4ee8ad46
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-02 16:13:23 +0300

    Update MulString.h

--- 2025/Seminars/Seminar. 07/MulString/MulString.h
+++ 2025/Seminars/Seminar. 07/MulString/MulString.h
@@ -1,1 +1,35 @@
+#include "MyString.h"
+#pragma once
+
+class MulString
+{
+private:
+
+	MyString str;
+
+public:
+
+	MulString(const MyString& str);
+
+	MulString(const char* data);
+
+	MulString& operator *= (int k);
+
+	MulString& operator %= (const MulString& other);
+
+	const char* getData() const;
+		 
+	friend std::ostream& operator << (std::ostream& os, const MulString s);
+
+};
+
+bool operator == (const MulString& s1, const MulString& s2);
+
+bool operator != (const MulString& s1, const MulString& s2);
+
+MulString operator * (const MulString& s, int k);
+
+MulString operator * (int k, const MulString& s);
+
+MulString operator % (const MulString& s1, const MulString& s2);
 

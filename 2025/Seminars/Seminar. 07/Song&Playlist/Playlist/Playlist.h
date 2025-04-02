379d09997fe4d99be30f66366ef5f5171be69b6f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-02 13:35:47 +0300

    Update Playlist.h

--- 2025/Seminars/Seminar. 07/Song&Playlist/Playlist/Playlist.h
+++ 2025/Seminars/Seminar. 07/Song&Playlist/Playlist/Playlist.h
@@ -1,1 +1,42 @@
+#include "Song.h"
+#pragma once
 
+class Playlist
+{
+private:
+
+	Song* songs = nullptr;
+	int countOfSongs = 0;
+	int capacity = 8;
+
+	void free();
+
+	void copyFrom(const Playlist& other);
+
+	void resize(int newCapacity);
+
+public:
+
+	Playlist();
+
+	Playlist(const Playlist& other);
+
+	Playlist& operator = (const Playlist& other);
+
+	~Playlist();
+
+	void addSong(const Song& song);
+
+	void removeSong(const Song& song);
+
+	void removeSong(int index);
+
+	friend std::ostream& operator << (std::ostream& os, const Playlist& p);
+
+	friend std::istream& operator >> (std::istream& is, Playlist& p);
+
+	void writeToBinaryFile(const char* fileName) const;
+
+	void readFromBinaryFile(const char* fileName);
+
+};

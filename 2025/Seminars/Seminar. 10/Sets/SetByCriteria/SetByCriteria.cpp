a5c43b1fa6715396ab60df5f489c2931b2ce4014
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 00:07:28 +0300

    Update SetByCriteria.cpp

--- 2025/Seminars/Seminar. 10/Sets/SetByCriteria/SetByCriteria.cpp
+++ 2025/Seminars/Seminar. 10/Sets/SetByCriteria/SetByCriteria.cpp
@@ -1,1 +1,35 @@
+#include "SetByCriteria.h"
 
+SetByCriteria::SetByCriteria(int number, predicate func): BitSet(number) 
+{
+
+	funciton = func;
+	fillSet(number);
+
+}
+
+bool SetByCriteria::hasCurrentNumber(int number) const
+{
+
+	return hasNumber(number);
+
+}
+
+void SetByCriteria::print() const 
+{
+
+	printSet();
+
+}
+
+void SetByCriteria::fillSet(int bound) 
+{
+
+	for (int i = 0; i <= bound; i++)
+	{
+
+		if (funciton(i)) addNumber(i);
+
+	}
+
+}

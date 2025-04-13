9a5b7979f83756a0a00e1b8fbec0db65e937cfdb
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 00:07:17 +0300

    Update SetByCriteria.h

--- 2025/Seminars/Seminar. 10/Sets/SetByCriteria/SetByCriteria.h
+++ 2025/Seminars/Seminar. 10/Sets/SetByCriteria/SetByCriteria.h
@@ -1,1 +1,23 @@
+#include "BitSet.h"
+#pragma once
+
+typedef bool (*predicate)(int);
+
+class SetByCriteria: private BitSet
+{
+private:
+
+	predicate funciton = nullptr;
+
+	void fillSet(int bound);
+
+public:
+
+	SetByCriteria(int number, predicate func);
+
+	bool hasCurrentNumber(int number) const;
+
+	void print() const;
+
+};
 

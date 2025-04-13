025ae53f5e7943b6acdc9cf7906c27feeba4eddf
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 00:07:38 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/Sets/SetByCriteria/main.cpp
+++ 2025/Seminars/Seminar. 10/Sets/SetByCriteria/main.cpp
@@ -1,1 +1,36 @@
+#include "SetByCriteria.h"
+
+bool isPrime(int number) 
+{
+
+	for (int i = 2; i < number; i++)
+	{
+
+		if (number % i == 0) return false;
+
+	}
+
+	return true;
+
+}
+
+bool allTo(int number) 
+{
+
+	return true;
+
+}
+
+int main() 
+{
+
+	SetByCriteria s1(10, isPrime);
+	SetByCriteria s2(47, allTo);
+	s1.print();
+	s2.print();
+
+	return 0;
+
+}
+
 

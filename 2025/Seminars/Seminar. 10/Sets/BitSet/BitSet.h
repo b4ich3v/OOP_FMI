fa18ec00258e5dbe4b2a310ed3fe3072c039e9dc
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-13 23:54:49 +0300

    Update BitSet.h

--- 2025/Seminars/Seminar. 10/Sets/BitSet/BitSet.h
+++ 2025/Seminars/Seminar. 10/Sets/BitSet/BitSet.h
@@ -1,1 +1,64 @@
+#include <iostream>
+#include <utility>
+#pragma once
 
+namespace CONSTANTS 
+{
+
+	const int countOfBitsInOneByte = 8;
+
+}
+
+class BitSet
+{
+private:
+
+	uint8_t* data = nullptr;
+	int size = 0;
+	int capacity = 8;
+
+	void resize(int newCapacity);
+
+	void free();
+
+	void copyFrom(const BitSet& other);
+
+	void moveTo(BitSet&& other);
+
+	int getBucketIndex(int number) const;
+
+	int indexInBucket(int number) const;
+
+public:
+
+	BitSet();
+
+	BitSet(int maxNumber);
+
+	BitSet(const BitSet& other);
+
+	BitSet& operator = (const BitSet& other);
+
+	BitSet(BitSet&& other) noexcept;
+
+	BitSet& operator = (BitSet&& other) noexcept;
+
+	~BitSet();
+
+	BitSet& operator &= (const BitSet& other);
+
+	BitSet& operator |= (const BitSet& other);
+
+	void addNumber(int number);
+
+	void removeNumber(int number);
+
+	bool hasNumber(int number) const;
+	
+	void printSet() const;
+
+};
+
+BitSet operator & (const BitSet& s1, const BitSet& s2);
+
+BitSet operator | (const BitSet& s1, const BitSet& s2);

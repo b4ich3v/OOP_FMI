a00bc479c0dc6fa36e9cef6e6363f079c027824c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-13 23:55:09 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/Sets/BitSet/main.cpp
+++ 2025/Seminars/Seminar. 10/Sets/BitSet/main.cpp
@@ -1,1 +1,32 @@
+#include "BitSet.h"
+
+int main() 
+{
+
+	BitSet s1(100);
+	s1.addNumber(1);
+	s1.addNumber(13);
+	s1.addNumber(67);
+	s1.addNumber(3);
+	s1.addNumber(11);
+	
+	BitSet s2(3);
+	s2.addNumber(1);
+	s2.addNumber(13);
+	s2.addNumber(67);
+	s2.addNumber(56);
+	s2.addNumber(111);
+	s2.addNumber(0);
+
+	BitSet unionOfTwo = s1 | s2;
+	BitSet intersectionOfTwo = s1 & s2;
+
+	unionOfTwo.printSet();
+	std::cout << std::endl;
+	intersectionOfTwo.printSet();
+
+	return 0;
+
+}
+
 

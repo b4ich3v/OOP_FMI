2818530fb99f4c9aab3c90cb112fcfc09e584e98
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 20:29:17 +0300

    Create WrapperInt.h

---
+++ 2025/Seminars/Seminar. 09/WrapperInt/WrapperInt.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+
+class WrapperInt
+{
+private:
+
+	int number = 0;
+
+public:
+
+	WrapperInt() = delete;
+
+	WrapperInt(int number);
+
+	WrapperInt& operator += (const WrapperInt& other);
+
+	WrapperInt& operator -= (const WrapperInt& other);
+
+	WrapperInt& operator *= (const WrapperInt& other);
+
+	WrapperInt& operator /= (const WrapperInt& other);
+
+	WrapperInt& operator ^= (int degree);
+
+	friend std::ostream& operator << (std::ostream& os, const WrapperInt& number);
+
+	friend std::istream& operator >> (std::istream& is, WrapperInt& number);
+
+	explicit operator int();
+
+	explicit operator double();
+
+	explicit operator bool();
+
+	explicit operator char();
+
+};
+
+WrapperInt operator + (const WrapperInt& number1, const WrapperInt& number2);
+
+WrapperInt operator - (const WrapperInt& number1, const WrapperInt& number2);
+
+WrapperInt operator * (const WrapperInt& number1, const WrapperInt& number2);
+
+WrapperInt operator / (const WrapperInt& number1, const WrapperInt& number2);

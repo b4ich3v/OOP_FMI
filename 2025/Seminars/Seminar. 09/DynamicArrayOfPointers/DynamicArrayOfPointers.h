4fdadceb397c62a4e58a1d2f9e1643b2394f8579
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 21:25:26 +0300

    Update DynamicArrayOfPointers.h

--- 2025/Seminars/Seminar. 09/DynamicArrayOfPointers/DynamicArrayOfPointers.h
+++ 2025/Seminars/Seminar. 09/DynamicArrayOfPointers/DynamicArrayOfPointers.h
@@ -1,1 +1,44 @@
+#include "WrapperInt.h"
+#pragma once
+
+class DynamicArrayOfPointers
+{
+private:
+
+	WrapperInt** arrayOfInts = nullptr;
+	int countOfInts = 0;
+	int capacity = 8;
+
+	void resize(int newCapacity);
+
+	void copyFrom(const DynamicArrayOfPointers& other);
+
+	void moveTo(DynamicArrayOfPointers&& other);
+
+	void free();
+
+public:
+
+	DynamicArrayOfPointers();
+
+	DynamicArrayOfPointers(const DynamicArrayOfPointers& other);
+
+	DynamicArrayOfPointers(DynamicArrayOfPointers&& other) noexcept;
+
+	DynamicArrayOfPointers& operator = (const DynamicArrayOfPointers& other);
+
+	DynamicArrayOfPointers& operator = (DynamicArrayOfPointers&& other) noexcept;
+
+	~DynamicArrayOfPointers();
+
+	void addInt(const WrapperInt& number);
+
+	void removeInt(int index);
+
+	friend std::ostream& operator << (std::ostream& os, const DynamicArrayOfPointers& arr);
+
+	friend std::istream& operator >> (std::istream& is, DynamicArrayOfPointers& arr);
+
+};
+
 

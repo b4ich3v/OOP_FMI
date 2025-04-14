46cfa1ec8718bc30f9ca47f97db263c3b012977d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 17:19:09 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 09/MyString/main.cpp
+++ 2025/Seminars/Seminar. 09/MyString/main.cpp
@@ -1,1 +1,25 @@
+#include "MyString.h"
 
+int main()
+{
+
+    MyString str1("Yoan");
+    MyString str2(" Baychev");
+
+    str1 += str2;
+    str2 = str1;
+    std::cout << str1;
+    std::cout << str1;
+
+    MyString copyStr1 = str1;
+    std::cout << copyStr1;
+
+    MyString str;
+    std::cin >> str;
+    std::cout << str;
+
+    std::cout << (str1 <= str2);
+
+    return 0;
+
+}

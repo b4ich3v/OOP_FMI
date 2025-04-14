d37cbe67dcde76c3eefad7e990309bf9dbd53e55
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-14 17:23:24 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 09/MyString/main.cpp
+++ 2025/Seminars/Seminar. 09/MyString/main.cpp
@@ -5,6 +5,8 @@ int main()
 
     MyString str1("Yoan");
     MyString str2(" Baychev");
+    MyString str3("xValue example");
+    MyString&& xRef = std::move(str3);
 
     str1 += str2;
     str2 = str1;
@@ -18,7 +20,8 @@ int main()
     std::cin >> str;
     std::cout << str;
 
-    std::cout << (str1 <= str2);
+    std::cout << (str1 <= str2) << std::endl;
+    std::cout << xRef << std::endl;
 
     return 0;
 

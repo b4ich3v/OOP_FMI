724f874f3e24488da57fd76aafdbacc114cc5f86
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 23:45:39 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/main.cpp
+++ 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/main.cpp
@@ -1,1 +1,17 @@
+#include "PrintableDoubleArray.h"
+
+int main()
+{
+
+	double arr[5] = { 1 ,2 ,3 ,4 ,5 };
+	PrintableDoubleArray pd1(arr, 5);
+	std::cout << pd1 << std::endl;
+
+	PrintableDoubleArray pd2;
+	std::cin >> pd2;
+	std::cout << pd2 << std::endl;
+
+	return 0;
+
+}
 

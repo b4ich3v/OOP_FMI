6480de275dae5cfbf26d9c4775918e8ce72c433f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 23:40:35 +0300

    Update PrintableDoubleArray.h

--- 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/PrintableDoubleArray.h
+++ 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/PrintableDoubleArray.h
@@ -1,1 +1,14 @@
+#include "DoubleArray.h"
+#pragma once
+
+class PrintableDoubleArray: public DoubleArray
+{
+public:
+
+	friend std::istream& operator >> (std::istream& is, DoubleArray& db);
+
+	friend std::ostream& operator << (std::ostream& os, const DoubleArray& db);
+
+};
+
 

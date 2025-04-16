0cdbc660948cfd4f1581cedba048016995ef233e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-17 00:25:23 +0300

    Update ModifiableDoubleArray.h

--- 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/ModifiableDoubleArray.h
+++ 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/ModifiableDoubleArray.h
@@ -1,1 +1,20 @@
+#include "DoubleArray.h"
+#pragma once
+
+class ModifiableDoubleArray: public DoubleArray
+{
+public:
+
+	ModifiableDoubleArray();
+
+	ModifiableDoubleArray(double* array, int size);
+
+	void push_back(double number);
+
+	void pop_back();
+
+	const double last() const;
+
+};
+
 

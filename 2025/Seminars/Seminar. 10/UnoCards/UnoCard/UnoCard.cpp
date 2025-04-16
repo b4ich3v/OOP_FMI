0784293ce489a60392cbb7b4ef884ec3a0239381
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:05:17 +0300

    Update UnoCard.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.cpp
@@ -1,32 +1,10 @@
 #include "UnoCard.h"
 
-UnoCard::UnoCard(int number, Color color)
+UnoCard::UnoCard(Color color) : color(color) {}
+
+Color UnoCard::getColor() const 
 {
 
-	if (number < 0 || number > 9) throw std::logic_error("Error");
-
-	this->number = number;
-	this->color = color;
+	return color;
 
 }
-
-void UnoCard::printCard() const 
-{
-
-	std::cout << number << " ";
-
-	switch (color)
-	{
-
-	case Color::RED: std::cout << "RED"; break;
-	case Color::GREEN: std::cout << "GREEN"; break;
-	case Color::BLUE: std::cout << "BLUE"; break;
-	case Color::YELLOW: std::cout << "YELLOW"; break;
-	default: break;
-		
-	}
-
-	std::cout << std::endl;
-
-}
-

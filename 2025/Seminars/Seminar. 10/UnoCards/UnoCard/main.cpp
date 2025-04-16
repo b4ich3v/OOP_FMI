4089ade915cb20a3c0c6ea38485305fb5d94d119
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:05:27 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/UnoCard/main.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/UnoCard/main.cpp
@@ -3,25 +3,8 @@
 int main()
 {
 
-	try
-	{
-
-		UnoCard card1(3, Color::BLUE);
-		UnoCard card2(7, Color::RED);
-
-		card1.printCard();
-		card2.printCard();
-
-		UnoCard card3(-1, Color::GREEN);
-		card3.printCard();
-
-	}
-	catch (const std::exception& e)
-	{
-
-		std::cout << e.what() << std::endl;
-
-	}
+	UnoCard card(Color::BLUE);
+	std::cout << (int)card.getColor() << std::endl;
 
 	return 0;
 

820b443cc91d373a25e4efe3c5213fb1cd5d125a
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:00:47 +0300

    Update UnoCard.h

--- 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.h
+++ 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.h
@@ -1,1 +1,29 @@
+#include <iostream>
+#pragma once
+
+enum class Color
+{
+
+	RED = 0,
+	GREEN = 1,
+	BLUE = 2,
+	YELLOW = 3
+
+};
+
+class UnoCard
+{
+private:
+
+	Color color;
+	int number = 0;
+
+public:
+
+	UnoCard(int number, Color color);
+
+	void printCard() const;
+
+};
+
 

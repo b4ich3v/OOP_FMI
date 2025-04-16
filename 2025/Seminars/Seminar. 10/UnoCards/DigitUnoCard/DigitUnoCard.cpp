fff9a8e7085177f8a8f74e1eaa3824476776a84c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:12:48 +0300

    Update DigitUnoCard.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/DigitUnoCard.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/DigitUnoCard.cpp
@@ -1,1 +1,31 @@
+#include "DigitUnoCard.h"
 
+DigitUnoCard::DigitUnoCard(int number, Color color): UnoCard(color)
+{
+
+	if (number < 0 || number > 9) throw std::logic_error("Error");
+
+	this->number = number;
+
+}
+
+void DigitUnoCard::printDigitUnoCard() const 
+{
+
+	std::cout << number << " ";
+	Color color = getColor();
+
+	switch (color)
+	{
+
+	case Color::RED: std::cout << "RED"; break;
+	case Color::GREEN: std::cout << "GREEN"; break;
+	case Color::BLUE: std::cout << "BLUE"; break;
+	case Color::YELLOW: std::cout << "YELLOW"; break;
+	default: break;
+
+	}
+
+	std::cout << std::endl;
+
+}

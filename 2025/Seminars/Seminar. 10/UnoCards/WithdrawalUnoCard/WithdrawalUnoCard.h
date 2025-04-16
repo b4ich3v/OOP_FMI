b4bfd822f5584ae267121aab73315917b4233157
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:29:31 +0300

    Update WithdrawalUnoCard.h

--- 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/WithdrawalUnoCard.h
+++ 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/WithdrawalUnoCard.h
@@ -1,1 +1,26 @@
+#include "UnoCard.h"
+#pragma once
+
+enum class CountOfCardToBeDrawn
+{
+
+	FOUR = 0,
+	SIX =  1
+
+};
+
+class WithdrawalUnoCard: public UnoCard
+{
+private:
+
+	CountOfCardToBeDrawn count;
+
+public:
+
+	WithdrawalUnoCard(CountOfCardToBeDrawn count, Color color);
+
+	void printWithdrawalUnoCard() const;
+
+};
+
 

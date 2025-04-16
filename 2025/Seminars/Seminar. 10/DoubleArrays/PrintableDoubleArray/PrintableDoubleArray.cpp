b5b6d3b1cff896b0714f6347db4178f97ce10cfa
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 23:40:53 +0300

    Update PrintableDoubleArray.cpp

--- 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/PrintableDoubleArray.cpp
+++ 2025/Seminars/Seminar. 10/DoubleArrays/PrintableDoubleArray/PrintableDoubleArray.cpp
@@ -1,1 +1,40 @@
+#include "PrintableDoubleArray.h"
 
+std::istream& operator >> (std::istream& is, DoubleArray& db) 
+{
+
+	int size = 0;
+	is >> size;
+
+	double* data = new double[size];
+
+	for (int i = 0; i < size; i++)
+	{
+
+		is >> data[i];
+
+	}
+
+	DoubleArray newVector(data, size);
+	db = newVector;
+	delete[] data;
+
+	return is;
+
+}
+
+std::ostream& operator << (std::ostream& os, const DoubleArray& db) 
+{
+
+	os << db.getSize() << ' ';
+
+	for (int i = 0; i < db.getSize(); i++)
+	{
+
+		os << db[i] << ' ';
+
+	}
+
+	return os;
+
+}

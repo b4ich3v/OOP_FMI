dbb0ede7ee20c93e21db69c7c0c4fce421c09e01
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-17 00:25:47 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/main.cpp
+++ 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/main.cpp
@@ -1,1 +1,20 @@
+#include "ModifiableDoubleArray.h"
+
+int main()
+{
+
+	double array[5] = { 1, 2, 3, 4, 5 };
+	ModifiableDoubleArray md(array, 5);
+
+	md.push_back(1);
+	std::cout << md[5] << std::endl;
+
+	md.pop_back();
+	std::cout << md[4] << std::endl;
+
+	std::cout << md.last() << std::endl;
+
+	return 0;
+
+}
 

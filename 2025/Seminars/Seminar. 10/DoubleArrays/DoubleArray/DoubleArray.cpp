aad57fea75bc08dd463ba9da3a12fec46b802f7a
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 23:26:11 +0300

    Create DoubleArray.cpp

---
+++ 2025/Seminars/Seminar. 10/DoubleArrays/DoubleArray/DoubleArray.cpp
@@ -0,0 +1,155 @@
+#include "DoubleArray.h"
+
+void DoubleArray::copyFrom(const DoubleArray& other)
+{
+
+	this->array = new double[other.capacity];
+
+	for (int i = 0; i < other.size; i++)
+	{
+
+		this->array[i] = other.array[i];
+
+	}
+
+	this->size = other.size;
+	this->capacity = other.capacity;
+
+}
+
+void DoubleArray::free()
+{
+
+	delete[] array;
+	array = nullptr;
+	size = 0;
+
+}
+
+void DoubleArray::resize(int newCapacity)
+{
+
+	if (newCapacity <= capacity) return;
+
+	double* newArray = new double[newCapacity];
+
+	for (int i = 0; i < size; i++)
+	{
+
+		newArray[i] = array[i];
+
+	}
+
+	delete[] array;
+	array = newArray;
+	capacity = newCapacity;
+
+}
+
+void DoubleArray::setArray(double* array, int size)
+{
+
+	if (this->array == array || !array) throw std::logic_error("Error");
+
+	delete[] this->array;
+	this->array = new double[size];
+
+	for (int i = 0; i < size; i++)
+	{
+
+		this->array[i] = array[i];
+
+	}
+
+	this->size = size;
+
+}
+
+DoubleArray::DoubleArray()
+{
+
+	size = 0;
+	capacity = 8;
+	array = new double[capacity];
+
+}
+
+DoubleArray::DoubleArray(int size)
+{
+
+	this->array = new double[size] {0};
+	capacity = size;
+
+}
+
+DoubleArray::DoubleArray(const DoubleArray& other)
+{
+
+	copyFrom(other);
+
+}
+
+DoubleArray::DoubleArray(double* array, int size)
+{
+
+	setArray(array, size);
+	this->capacity = 8;
+
+}
+
+DoubleArray& DoubleArray::operator = (const DoubleArray& other)
+{
+
+	if (this != &other)
+	{
+
+		free();
+		copyFrom(other);
+
+	}
+
+	return *this;
+
+}
+
+DoubleArray::~DoubleArray()
+{
+
+	free();
+
+}
+
+void DoubleArray::moveTo(DoubleArray&& other) 
+{
+
+	this->array = other.array;
+	this->capacity = other.capacity;
+	this->size = other.size;
+
+	other.array = nullptr;
+	other.capacity = 8;
+	other.size = 0;
+
+}
+
+DoubleArray::DoubleArray(DoubleArray&& other) noexcept 
+{
+
+	moveTo(std::move(other));
+
+}
+
+DoubleArray& DoubleArray::operator = (DoubleArray&& other) noexcept 
+{
+
+	if (this != &other) 
+	{
+
+		free();
+		moveTo(std::move(other));
+
+	}
+
+	return *this;
+
+}

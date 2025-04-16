312b10d6d5b313cb7350d2d09383e9ed463cd6d4
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 13:31:09 +0300

    Update Person.h

--- 2025/Seminars/Seminar. 10/StudentAndTeacher/Person/Person.h
+++ 2025/Seminars/Seminar. 10/StudentAndTeacher/Person/Person.h
@@ -1,1 +1,40 @@
+#include <cstring>
+#include <utility>
+#pragma once
+
+class Person
+{
+private:
+
+	char* name = nullptr;
+	int age = 0;
+
+	void free();
+
+	void copyFrom(const Person& other);
+
+	void moveTo(Person&& other);
+
+public:
+
+	Person() = default;
+
+	Person(const Person& other);
+
+	Person(const char* name, int age);
+
+	Person(Person&& other) noexcept;
+
+	Person& operator = (const Person& other);
+
+	Person& operator = (Person&& other) noexcept;
+
+	~Person();
+
+	const char* getName() const;
+
+	int getAge() const;
+
+};
+
 

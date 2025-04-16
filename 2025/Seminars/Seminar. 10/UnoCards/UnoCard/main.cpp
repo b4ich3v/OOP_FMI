1811563e5e1fcd56f1dc6883872a2198a244cf16
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:01:07 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/UnoCard/main.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/UnoCard/main.cpp
@@ -1,1 +1,28 @@
+#include "UnoCard.h"
 
+int main()
+{
+
+	try
+	{
+
+		UnoCard card1(3, Color::BLUE);
+		UnoCard card2(7, Color::RED);
+
+		card1.printCard();
+		card2.printCard();
+
+		UnoCard card3(-1, Color::GREEN);
+		card3.printCard();
+
+	}
+	catch (const std::exception& e)
+	{
+
+		std::cout << e.what() << std::endl;
+
+	}
+
+	return 0;
+
+}

0f29422b63bd0c6a7d392acc74d22c897d9b76cc
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 17:41:39 +0300

    Update UnoCard.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/UnoCard/UnoCard.cpp
@@ -8,3 +8,31 @@ Color UnoCard::getColor() const
 	return color;
 
 }
+
+bool UnoCard::isDigitUnoCard(const UnoCard* ptr) const 
+{
+
+	return (*ptr).isDigitUnoCard(ptr);
+
+}
+
+bool UnoCard::isSpecialUnoCard(const UnoCard* ptr) const 
+{
+
+	return (*ptr).isSpecialUnoCard(ptr);
+
+}
+
+bool UnoCard::isWithdrawalUnoCard(const UnoCard* ptr) const 
+{
+
+	return (*ptr).isWithdrawalUnoCard(ptr);
+
+}
+
+void UnoCard::printCard(const UnoCard* ptr) const 
+{
+
+	(*ptr).printCard(ptr);
+
+}

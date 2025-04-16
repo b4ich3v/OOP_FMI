a068c82b8cc7a8395f2c3453475079216059fedd
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 13:42:01 +0300

    Update Student.cpp

--- 2025/Seminars/Seminar. 10/StudentAndTeacher/Student/Student.cpp
+++ 2025/Seminars/Seminar. 10/StudentAndTeacher/Student/Student.cpp
@@ -1,1 +1,30 @@
+#include "Student.h"
+
+Student::Student(const char* name, int age, const char* id): Person(name, age)
+{
+
+    strncpy(this->id, id, strlen(id));
+
+}
+
+const char* Student::getStudentName() const
+{
+
+    return getName();
+
+}
+
+int Student::getStudentAge() const
+{
+
+    return getAge();
+
+}
+
+const char* Student::getStudentId() const
+{
+
+    return id;
+
+}
 

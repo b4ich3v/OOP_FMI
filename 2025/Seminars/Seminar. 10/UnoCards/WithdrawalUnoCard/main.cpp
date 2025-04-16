69a0f4a2776cdbfb3beb072823cf2f8dbdf7a85b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:29:53 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/main.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/main.cpp
@@ -1,1 +1,13 @@
+#include "WithdrawalUnoCard.h"
 
+int main()
+{
+
+	WithdrawalUnoCard card1(CountOfCardToBeDrawn::FOUR, Color::BLUE);
+	WithdrawalUnoCard card2(CountOfCardToBeDrawn::SIX, Color::RED);
+	card1.printWithdrawalUnoCard();
+	card2.printWithdrawalUnoCard();
+
+	return 0;
+
+}

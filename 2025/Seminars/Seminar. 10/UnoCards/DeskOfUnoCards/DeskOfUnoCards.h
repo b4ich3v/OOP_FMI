908f0499c06c9f5cda9aff1cf926f9d79b1a4930
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 17:43:24 +0300

    Update DeskOfUnoCards.h

--- 2025/Seminars/Seminar. 10/UnoCards/DeskOfUnoCards/DeskOfUnoCards.h
+++ 2025/Seminars/Seminar. 10/UnoCards/DeskOfUnoCards/DeskOfUnoCards.h
@@ -1,1 +1,43 @@
+#include "UnoCard.h"
+#include "DigitUnoCard.h"
+#pragma once
+
+class DeskOfUnoCards
+{
+private:
+
+	UnoCard** cards = nullptr;
+	int countOfCards = 0;
+	int capacity = 8;
+
+	void free();
+
+	void resize(int newCapacity);
+
+	void copyFrom(const DeskOfUnoCards& other);
+
+	void moveTo(DeskOfUnoCards&& other);
+
+public:
+
+	DeskOfUnoCards();
+
+	DeskOfUnoCards(const DeskOfUnoCards& other);
+
+	DeskOfUnoCards(DeskOfUnoCards&& other) noexcept;
+
+	DeskOfUnoCards& operator = (const DeskOfUnoCards& other);
+
+	DeskOfUnoCards& operator = (DeskOfUnoCards&& other) noexcept;
+
+	~DeskOfUnoCards();
+
+	void addCard(const UnoCard* card);
+
+	void removeCard(const DigitUnoCard& card);
+
+	void printDesk() const;
+
+};
+
 

39138d84ce6b8876c7b114ebcaa477565014dbdf
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 13:31:30 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/StudentAndTeacher/Person/main.cpp
+++ 2025/Seminars/Seminar. 10/StudentAndTeacher/Person/main.cpp
@@ -1,1 +1,19 @@
+#include "Person.h"
+#include <iostream>
 
+int main()
+{
+
+	Person p1("Yoan Baychev", 20);
+	Person p2("Ilian Zaprqnov", 21);
+	Person p3("Kaloqn Markov", 21);
+	Person p4("Ivailo Kunchev", 20);
+
+	std::cout << p1.getName() << " " << p1.getAge() << std::endl;
+	std::cout << p2.getName() << " " << p1.getAge() << std::endl;
+	std::cout << p3.getName() << " " << p1.getAge() << std::endl;
+	std::cout << p4.getName() << " " << p1.getAge() << std::endl;
+
+	return 0;
+
+}

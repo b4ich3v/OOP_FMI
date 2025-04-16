16d5124d543c46396de10a07cfea28410eb6fc59
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 17:47:01 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/main.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/main.cpp
@@ -8,11 +8,11 @@ int main()
 
 		DigitUnoCard card1(3, Color::BLUE);
 		DigitUnoCard card2(7, Color::RED);
-		card1.printDigitUnoCard();
-		card2.printDigitUnoCard();
+		card1.printCard(&card1);
+		card2.printCard(&card2);
 
 		DigitUnoCard card3(-1, Color::GREEN);
-		card3.printDigitUnoCard();
+		card3.printCard(&card3);
 
 	}
 	catch (const std::exception& e)

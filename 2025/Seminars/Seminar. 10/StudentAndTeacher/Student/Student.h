e0f27787955ab6895e674a2d1274cf95d446de44
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 13:41:28 +0300

    Update Student.h

--- 2025/Seminars/Seminar. 10/StudentAndTeacher/Student/Student.h
+++ 2025/Seminars/Seminar. 10/StudentAndTeacher/Student/Student.h
@@ -1,1 +1,24 @@
+#include "Person.h"
+
+const int ID_SIZE = 10;
+
+class Student: public Person
+{
+private:
+
+    char id[ID_SIZE + 1] = "0MI060032";
+
+public:
+
+    Student() = default;
+
+    Student(const char* name, int age, const char* id);
+
+    const char* getStudentName() const;
+
+    int getStudentAge() const;
+
+    const char* getStudentId() const;
+
+};
 

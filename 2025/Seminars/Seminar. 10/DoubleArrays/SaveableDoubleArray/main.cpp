6480f38553fe137b176e3ce89699cdb581275668
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-17 00:00:43 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/DoubleArrays/SaveableDoubleArray/main.cpp
+++ 2025/Seminars/Seminar. 10/DoubleArrays/SaveableDoubleArray/main.cpp
@@ -1,1 +1,32 @@
+#include "SaveableDoubleArray.h"
+
+int main()
+{
+
+	double arr[5] = { 1 ,2 ,3 ,4 ,5 };
+	SaveableDoubleArray sd1(arr, 5);
+
+	try
+	{
+
+		std::ofstream file1("result.dat", std::ios::binary);
+		sd1.saveToFile(file1);
+
+		std::ifstream file2("result.dat", std::ios::binary);
+		SaveableDoubleArray sd2;
+		sd2.readFromFile(file2);
+
+		std::cout << (sd1 == sd2) << std::endl;
+
+	}
+	catch (const std::exception& e)
+	{
+
+		std::cout << e.what() << std::endl;
+
+	}
+
+	return 0;
+
+}
 

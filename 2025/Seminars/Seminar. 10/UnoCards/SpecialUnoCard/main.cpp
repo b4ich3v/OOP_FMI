26e08561f9153dfad87e5a5ac229296c24c12743
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:21:17 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/SpecialUnoCard/main.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/SpecialUnoCard/main.cpp
@@ -1,1 +1,13 @@
+#include "SpecialUnoCard.h"
 
+int main()
+{
+
+	SpecialUnoCard card1(Action::REVERSE, Color::BLUE);
+	SpecialUnoCard card2(Action::SKIP, Color::RED);
+	card1.printSpecialUnoCard();
+	card2.printSpecialUnoCard();
+
+	return 0;
+
+}

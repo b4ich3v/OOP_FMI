53958a947d05046daf875d2c22589fe26510f3e5
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:21:07 +0300

    Update SpecialUnoCard.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/SpecialUnoCard/SpecialUnoCard.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/SpecialUnoCard/SpecialUnoCard.cpp
@@ -1,1 +1,32 @@
+#include "SpecialUnoCard.h"
 
+SpecialUnoCard::SpecialUnoCard(Action action, Color color): UnoCard(color), action(action) {}
+
+void SpecialUnoCard::printSpecialUnoCard() const 
+{
+
+	switch (action)
+	{
+
+	case Action::SKIP: std::cout << "SKIP "; break;
+	case Action::REVERSE: std::cout << "REVERSE "; break;
+	default: break;
+
+	}
+
+	Color color = getColor();
+
+	switch (color)
+	{
+
+	case Color::RED: std::cout << "RED"; break;
+	case Color::GREEN: std::cout << "GREEN"; break;
+	case Color::BLUE: std::cout << "BLUE"; break;
+	case Color::YELLOW: std::cout << "YELLOW"; break;
+	default: break;
+
+	}
+
+	std::cout << std::endl;
+
+}

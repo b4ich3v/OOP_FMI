73925c556094c63ca0005917759b3eb452f2da1e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:12:33 +0300

    Update DigitUnoCard.h

--- 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/DigitUnoCard.h
+++ 2025/Seminars/Seminar. 10/UnoCards/DigitUnoCard/DigitUnoCard.h
@@ -1,1 +1,18 @@
+#include "UnoCard.h"
+#pragma once
+
+class DigitUnoCard: public UnoCard
+{
+private:
+
+	int number = 0;
+
+public:
+
+	DigitUnoCard(int number, Color color);
+
+	void printDigitUnoCard() const;
+
+};
+
 

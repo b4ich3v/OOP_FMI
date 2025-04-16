24a5982bc90d67a26e51ad1871d62390eb24d784
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-17 00:00:17 +0300

    Update SaveableDoubleArray.h

--- 2025/Seminars/Seminar. 10/DoubleArrays/SaveableDoubleArray/SaveableDoubleArray.h
+++ 2025/Seminars/Seminar. 10/DoubleArrays/SaveableDoubleArray/SaveableDoubleArray.h
@@ -1,1 +1,19 @@
+#include <fstream>
+#include "DoubleArray.h"
+#pragma once
+
+class SaveableDoubleArray: public DoubleArray
+{
+public:
+
+	SaveableDoubleArray();
+
+	SaveableDoubleArray(double* array, int size);
+
+	void saveToFile(std::ofstream& file) const;
+
+	void readFromFile(std::ifstream& file);
+
+};
+
 

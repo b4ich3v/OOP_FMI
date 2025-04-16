cb4bd0e095394b39f3ab312e8e0232708e9e5c2f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-16 16:29:43 +0300

    Update WithdrawalUnoCard.cpp

--- 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/WithdrawalUnoCard.cpp
+++ 2025/Seminars/Seminar. 10/UnoCards/WithdrawalUnoCard/WithdrawalUnoCard.cpp
@@ -1,1 +1,32 @@
+#include "WithdrawalUnoCard.h"
 
+WithdrawalUnoCard::WithdrawalUnoCard(CountOfCardToBeDrawn count, Color color): UnoCard(color), count(count) {}
+
+void WithdrawalUnoCard::printWithdrawalUnoCard() const 
+{
+
+	switch (count)
+	{
+
+	case CountOfCardToBeDrawn::FOUR: std::cout << "FOUR "; break;
+	case CountOfCardToBeDrawn::SIX: std::cout << "SIX "; break;
+	default: break;
+		
+	}
+
+	Color color = getColor();
+
+	switch (color)
+	{
+
+	case Color::RED: std::cout << "RED"; break;
+	case Color::GREEN: std::cout << "GREEN"; break;
+	case Color::BLUE: std::cout << "BLUE"; break;
+	case Color::YELLOW: std::cout << "YELLOW"; break;
+	default: break;
+
+	}
+
+	std::cout << std::endl;
+
+}

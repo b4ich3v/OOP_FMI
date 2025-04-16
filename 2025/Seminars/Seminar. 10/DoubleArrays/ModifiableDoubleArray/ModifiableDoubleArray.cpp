52f3ecee880539ee1210bf522e8c4f14df1debf2
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-17 00:25:32 +0300

    Update ModifiableDoubleArray.cpp

--- 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/ModifiableDoubleArray.cpp
+++ 2025/Seminars/Seminar. 10/DoubleArrays/ModifiableDoubleArray/ModifiableDoubleArray.cpp
@@ -1,1 +1,38 @@
+#include "ModifiableDoubleArray.h"
 
+ModifiableDoubleArray::ModifiableDoubleArray(): DoubleArray() {}
+
+ModifiableDoubleArray::ModifiableDoubleArray(double* array, int size): DoubleArray(array, size) {}
+
+void ModifiableDoubleArray::push_back(double number) 
+{
+
+	if (getSize() == getCapacity()) publicResize();
+
+	int lastIndex = getSize();
+	double* array = getArr();
+
+	array[lastIndex] = number;
+	incrementSize();
+
+}
+
+void ModifiableDoubleArray::pop_back() 
+{
+
+	if (getSize() == 0) return;
+
+	decrementSize();
+
+}
+
+const double ModifiableDoubleArray::last() const 
+{
+
+	if (getSize() == 0) throw std::logic_error("Error");
+
+	const double* array = getArr();
+
+	return array[getSize() - 1];
+
+}

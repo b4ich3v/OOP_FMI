062c2da4ff0004d3ff8ef5b154e15a3faa3b197c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-28 16:52:09 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/DrinksAndBar/Drink/main.cpp
+++ 2025/Seminars/Seminar. 10/DrinksAndBar/Drink/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "Drink.h" 
+
+int main() 
+{
+    
+    Drink water("Water", 0.5);
+    std::cout << water.getName() << " " << water.getQuantity() << std::endl;
+
+    Drink juice = water;
+    std::cout << juice.getName() << " " << juice.getQuantity() << std::endl;
+
+    Drink soda = Drink("Soda", 1.0);
+    std::cout << soda.getName() << " " << soda.getQuantity() << std::endl;
+
+    Drink coffee;
+    coffee = water;
+    std::cout << coffee.getName() << " " << coffee.getQuantity() << std::endl;
+
+    Drink tea;
+    tea = Drink("Tea", 0.3);
+    std::cout << tea.getName() << " " << tea.getQuantity() << std::endl;
+
+    return 0;
+
+}
 

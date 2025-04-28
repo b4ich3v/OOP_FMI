ab48d2bd4361b484a49ae9d9076a337a8b89299d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-28 16:59:14 +0300

    Update AlcoholicDrink.h

--- 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/AlcoholicDrink.h
+++ 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/AlcoholicDrink.h
@@ -1,1 +1,18 @@
+#include "Drink.h"
+#pragma once
+
+class AlcoholicDrink: public Drink
+{
+private:
+
+	double alcPer = 0;
+
+public:
+
+	AlcoholicDrink(const char* name, double quantity, double alcPer);
+
+	double getAlcPer() const;
+
+};
+
 

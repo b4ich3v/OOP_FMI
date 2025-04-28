e025b3a04118f4ac7d2b75ab43d4034d5f4d168b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-28 16:59:23 +0300

    Update AlcoholicDrink.cpp

--- 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/AlcoholicDrink.cpp
+++ 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/AlcoholicDrink.cpp
@@ -1,1 +1,17 @@
+#include "AlcoholicDrink.h"
+
+AlcoholicDrink::AlcoholicDrink(const char* name, double quantity, 
+	double alcPer) : Drink(name, quantity) 
+{
+
+	this->alcPer = alcPer;
+
+}
+
+double AlcoholicDrink::getAlcPer() const 
+{
+
+	return alcPer;
+
+}
 

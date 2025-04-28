ab1427dceadb9da0de5faf55fe7453296b6d9a5c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-28 16:46:54 +0300

    Update Drink.h

--- 2025/Seminars/Seminar. 10/DrinksAndBar/Drink/Drink.h
+++ 2025/Seminars/Seminar. 10/DrinksAndBar/Drink/Drink.h
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <cstring>
+#pragma once
+
+class Drink
+{
+private:
+
+	char* name = nullptr;
+	double quantity = 0;
+
+	void free();
+
+	void copyFrom(const Drink& other);
+
+	void moveTo(Drink&& other);
+
+public:
+
+	Drink() = default;
+
+	Drink(const char* name, double quantity);
+
+	Drink(const Drink& other);
+
+	Drink(Drink&& other) noexcept;
+
+	Drink& operator = (const Drink& other);
+
+	Drink& operator = (Drink&& other) noexcept;
+
+	~Drink();
+
+	const char* getName() const;
+
+	double getQuantity() const;
+
+};
+
 

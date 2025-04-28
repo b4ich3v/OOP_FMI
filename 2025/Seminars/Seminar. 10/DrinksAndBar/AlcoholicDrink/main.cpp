5b188053c4428dccb8986d7c3476b3bba3abf6f0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-28 16:59:35 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/main.cpp
+++ 2025/Seminars/Seminar. 10/DrinksAndBar/AlcoholicDrink/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "AlcoholicDrink.h"
+
+int main() 
+{
+    
+    AlcoholicDrink wine("Wine", 0.75, 12.5);
+    std::cout << wine.getName() << " " << wine.getQuantity() << " "
+        << wine.getAlcPer() << std::endl;
+
+    AlcoholicDrink beer("Beer", 0.5, 5.0);
+
+    std::cout << beer.getName() << " " << beer.getQuantity() << " "
+        << beer.getAlcPer() << std::endl;
+
+    return 0;
+
+}
 

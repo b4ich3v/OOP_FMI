5b47d8bf7bec5cbf5ee8f50da61ff4748d6f8621
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 21:50:24 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 13/Employees/Engineer/main.cpp
+++ 2025/Seminars/Seminar. 13/Employees/Engineer/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "Engineer.h"
+
+int main()
+{
+
+    MyString name("Ivan Petrov");
+    MyString tech("Machine Learning");
+
+    Engineer eg(name, tech);
+    eg.printInfo();
+
+    return 0;
+}
 

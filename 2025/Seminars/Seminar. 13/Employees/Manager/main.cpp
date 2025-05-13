a30186e8e6bac80c3d59f83ccb9342eb26c6a743
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 21:49:37 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 13/Employees/Manager/main.cpp
+++ 2025/Seminars/Seminar. 13/Employees/Manager/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "Manager.h"
+
+int main()
+{
+
+    MyString name("Ivan Petrov");
+    MyString project("AI Assistant");
+
+    Manager mg(name, project);
+    mg.printInfo();
+
+    return 0;
+}
 

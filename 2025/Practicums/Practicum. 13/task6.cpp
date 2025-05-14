3f9e3c1ddd17e801e31897d0f177bee655b418f0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-15 01:47:58 +0300

    Update task6.cpp

--- 2025/Practicums/Practicum. 13/task6.cpp
+++ 2025/Practicums/Practicum. 13/task6.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#pragma once
 
 template <class T>
 
@@ -311,3 +310,49 @@ std::istream& operator >> (std::istream& is, MyVector<T>& v)
 
 }
 
+int main()
+{
+
+    MyVector<int> v;
+    std::cout  << v.empty() << std::endl;
+
+    for (int i = 1; i <= 5; i++)
+    {
+
+        v.push_back(i * 10);
+
+    }
+
+    std::cout << v << std::endl;
+    std::cout << v.size() << std::endl << std::endl;
+
+    MyVector<int> copy = v;
+    std::cout << copy << std::endl;
+
+    MyVector<int> moved = std::move(v);
+    std::cout << moved << std::endl;
+    std::cout << v.empty() << std::endl << std::endl;
+
+    try
+    {
+
+        std::cout << moved[2] << std::endl;
+        moved.pop_back();
+        std::cout << moved << std::endl;
+
+    }
+    catch (const std::out_of_range& e)
+    {
+
+        std::cout << e.what() << std::endl;
+
+    }
+    std::cout << std::endl;
+
+    MyVector<int> inputVec;
+    std::cin >> inputVec;
+    std::cout << inputVec << std::endl;
+
+    return 0;
+
+}

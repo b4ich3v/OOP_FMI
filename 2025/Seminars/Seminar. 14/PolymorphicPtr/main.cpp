087161b645e2aab51cd23acec66eb5b2c206b9b2
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-28 01:54:38 +0300

    Update main.cpp

--- 2025/Seminars/Seminar. 14/PolymorphicPtr/main.cpp
+++ 2025/Seminars/Seminar. 14/PolymorphicPtr/main.cpp
@@ -1,1 +1,65 @@
+#include "PolymorphicPtr.hpp"
+
+class Base
+{
+public:
+
+	virtual Base* clone() const = 0;
+
+	virtual ~Base() = default;
+
+};
+
+class Der1 : public Base
+{
+public:
+
+	void der1() const
+	{
+
+		std::cout << "Der1" << std::endl;
+
+	}
+
+	Base* clone() const override
+	{
+
+		return new Der1();
+
+	}
+
+};
+
+class Der2 : public Base
+{
+public:
+
+	void der2() const
+	{
+
+		std::cout << "Der2" << std::endl;
+
+	}
+
+	Base* clone() const override
+	{
+
+		return new Der2();
+
+	}
+
+};
+
+int main()
+{
+
+	PolymorphicPtr<Der1> ptr1(new Der1());
+	ptr1->der1();
+
+	PolymorphicPtr<Der2> ptr2(new Der2());
+	ptr2->der2();
+
+	return 0;
+
+}
 
